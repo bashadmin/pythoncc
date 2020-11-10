9e1734eb484842a0745d53cd82c8efda268a2d7d
bashadmin <dev.sugarman@gmail.com>
2020-11-10 00:03:36 -0500

    Minor edits.

--- pylabs/pycc02.py
+++ pylabs/pycc02.py
@@ -107,3 +107,13 @@ def skip_elements2(elements):
 
 print(skip_elements2(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
 print(skip_elements2(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
+
+
+def odd_numbers(n):
+	return [x for x in range(1,n+1) if x % 2 != 0]
+
+print(odd_numbers(5))  # Should print [1, 3, 5]
+print(odd_numbers(10)) # Should print [1, 3, 5, 7, 9]
+print(odd_numbers(11)) # Should print [1, 3, 5, 7, 9, 11]
+print(odd_numbers(1))  # Should print [1]
+print(odd_numbers(-1)) # Should print []
